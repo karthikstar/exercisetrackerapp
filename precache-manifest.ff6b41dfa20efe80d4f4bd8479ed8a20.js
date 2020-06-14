2a3b89a5fac2e99cce2ff28c0ed758ce2423c9bc
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2020-06-14 17:22:16 +0800

    Updates

---
+++ precache-manifest.ff6b41dfa20efe80d4f4bd8479ed8a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de31f5bbbdf3c3e1d10bff35ece09856",
+    "url": "/exercisetracker/index.html"
+  },
+  {
+    "revision": "2dca61ea2dc5ca95c9ef",
+    "url": "/exercisetracker/static/css/2.bf5b17f7.chunk.css"
+  },
+  {
+    "revision": "bd942400aecc9b70c329",
+    "url": "/exercisetracker/static/css/main.837f8ad9.chunk.css"
+  },
+  {
+    "revision": "2dca61ea2dc5ca95c9ef",
+    "url": "/exercisetracker/static/js/2.c3048dee.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exercisetracker/static/js/2.c3048dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd942400aecc9b70c329",
+    "url": "/exercisetracker/static/js/main.bc7e441f.chunk.js"
+  },
+  {
+    "revision": "c16bd5e08f0d9b887c37",
+    "url": "/exercisetracker/static/js/runtime-main.ae360901.js"
+  }
+]);
